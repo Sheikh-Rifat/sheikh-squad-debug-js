2c400f5f28983bd8c77f4fd4aa929319a1e95eb2
Sheikh-Rifat <brotherssk57@gmail.com>
2021-09-13 22:17:27 +0600

    added fontawesome and completed bonus part

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ const showProducts = (products) => {
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
-      <h4>Rating: <span class="rating"> ${product.rating.rate} </span> (${product.rating.count})</h4>
+      <h4>Rating: <span class="rating"><i class="fas fa-star"></i> ${product.rating.rate} </span> (${product.rating.count})</h4>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
